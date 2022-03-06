30b4503299fc0106887b6af5c5aa682b5d3c0eed
sbalayan1 <balayans2014@yahoo.com>
2022-03-06 01:03:27 -0800

    working

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,13 @@
+require 'pry'
+
 class TicTacToe 
     def initialize
-        @board = Array.new(9).fill(' ')
+        @board = Array.new(9).fill(" ")
     end 
 
     WIN_COMBINATIONS = [
         [0,1,2],
-        [3,4,5,
+        [3,4,5],
         [6,7,8],
         [0,3,6],
         [1,4,7],
@@ -13,4 +15,17 @@ class TicTacToe
         [0,4,8],
         [6,4,2] 
     ]
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " 
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} " 
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
+    end
+
+    def input_to_index number
+        number = number.to_i - 1
+    end 
+
 end 
